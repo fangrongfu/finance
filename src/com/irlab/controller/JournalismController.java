5e1910396692e03fb0041ca6d131b39d9b2e09a8
fangrongfu <251591587@qq.com>
2017-08-21 16:02:51 +0800

    测试新闻显示样式

--- src/com/irlab/controller/JournalismController.java
+++ src/com/irlab/controller/JournalismController.java
@@ -3,7 +3,9 @@
  */
 package com.irlab.controller;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -26,13 +28,22 @@ public class JournalismController {
 	public Map<String, Object> selectJournalismController(){
 		Map<String, Object> map = new HashMap<String, Object>();
 		Journalism journalism = new Journalism();
+		/*journalism.setJ_id(000413);
+		journalism.setJ_title("公司顺利通过了SGS进行TS16949初步审核");
+		journalism.setJ_time("2017.03.01");
+		journalism.setJ_digest("经过近半年的前期学习、准备，2014年9月22日，认证机构SGS前来我公司进行TS16949管理体系的初步审核，确认基本符合要求，预定于10月中旬进行正式审核。TS16949管理体系认证是汽车行业质量管理的基本要求，推行该体系旨在增强公司向汽车用产品供货的质量保证能力。");
+		journalism.setJ_industry("电器设备");
+		journalism.setJ_url("http://www.szxinye.com/info.php?mod=detail&id=5");*/
+		List<Journalism> m = new ArrayList<Journalism>();
 		journalism.setJ_id(000413);
 		journalism.setJ_title("公司顺利通过了SGS进行TS16949初步审核");
 		journalism.setJ_time("2017.03.01");
 		journalism.setJ_digest("经过近半年的前期学习、准备，2014年9月22日，认证机构SGS前来我公司进行TS16949管理体系的初步审核，确认基本符合要求，预定于10月中旬进行正式审核。TS16949管理体系认证是汽车行业质量管理的基本要求，推行该体系旨在增强公司向汽车用产品供货的质量保证能力。");
 		journalism.setJ_industry("电器设备");
 		journalism.setJ_url("http://www.szxinye.com/info.php?mod=detail&id=5");
-		map.put("journalism",journalism);
+		m.add(journalism);
+		map.put("rows",m);
+		map.put("total", 2);
 		System.out.println(map);
 		return map;
 	}
