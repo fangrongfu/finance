aa85db348ba83efa2d24c00fe4e3ab4086b8cb9a
fangrongfu <251591587@qq.com>
2017-08-22 16:49:50 +0800

    添加了新闻的修改

---
+++ src/com/irlab/service/impl/JournalismService.java
@@ -0,0 +1,14 @@
+package com.irlab.service.impl;
+
+import java.util.Map;
+
+import com.irlab.entity.Paging;
+
+/**
+ * @author  fangrongfu
+ * @version 1.0
+ * @time    2017年8月21日下午10:26:28
+ */
+public interface JournalismService {
+	public Map<String,Object> selectJournalismService(Paging paging);	//新闻检索的业务处理接口
+}
