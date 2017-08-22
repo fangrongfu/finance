2941d5ead04c5d9f0b9ddfaebb9c3252c3ae0b0d
fangrongfu <251591587@qq.com>
2017-08-22 16:43:09 +0800

    修改了新闻部分，更新了新闻数据

--- src/com/irlab/controller/JournalismController.java
+++ src/com/irlab/controller/JournalismController.java
@@ -8,11 +8,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.irlab.dao.JournalismDao;
 import com.irlab.entity.Journalism;
+import com.irlab.entity.Paging;
+import com.irlab.service.impl.JournalismService;
 
 /**
  * @author  fangrongfu
@@ -23,28 +29,31 @@ import com.irlab.entity.Journalism;
 @RequestMapping("/journalism")
 public class JournalismController {
 	
+	@Resource
+	private JournalismService journalismService;
+	
+	@Resource
+	private JournalismDao journalismDao;
+	
 	@RequestMapping("/select")
 	@ResponseBody
-	public Map<String, Object> selectJournalismController(){
+	public Map<String, Object> selectJournalismController(HttpServletRequest request,int page,int rows,String searchObj,String date){
+		Paging paging = new Paging();
 		Map<String, Object> map = new HashMap<String, Object>();
-		Journalism journalism = new Journalism();
-		/*journalism.setJ_id(000413);
-		journalism.setJ_title("公司顺利通过了SGS进行TS16949初步审核");
-		journalism.setJ_time("2017.03.01");
-		journalism.setJ_digest("经过近半年的前期学习、准备，2014年9月22日，认证机构SGS前来我公司进行TS16949管理体系的初步审核，确认基本符合要求，预定于10月中旬进行正式审核。TS16949管理体系认证是汽车行业质量管理的基本要求，推行该体系旨在增强公司向汽车用产品供货的质量保证能力。");
-		journalism.setJ_industry("电器设备");
-		journalism.setJ_url("http://www.szxinye.com/info.php?mod=detail&id=5");*/
-		List<Journalism> m = new ArrayList<Journalism>();
-		journalism.setJ_id(000413);
-		journalism.setJ_title("公司顺利通过了SGS进行TS16949初步审核");
-		journalism.setJ_time("2017.03.01");
-		journalism.setJ_digest("经过近半年的前期学习、准备，2014年9月22日，认证机构SGS前来我公司进行TS16949管理体系的初步审核，确认基本符合要求，预定于10月中旬进行正式审核。TS16949管理体系认证是汽车行业质量管理的基本要求，推行该体系旨在增强公司向汽车用产品供货的质量保证能力。");
-		journalism.setJ_industry("电器设备");
-		journalism.setJ_url("http://www.szxinye.com/info.php?mod=detail&id=5");
-		m.add(journalism);
-		map.put("rows",m);
-		map.put("total", 2);
-		System.out.println(map);
-		return map;
+		if(date.equals("all")) {
+			date = null;
+			paging.setJ_time(date);
+			paging.setJ_title(searchObj);
+			paging.setJ_content(searchObj);
+			paging.setN_start((page-1)*rows);
+			paging.setN_rows(rows);
+		}else {
+			paging.setJ_time(date);
+			paging.setJ_title(searchObj);
+			paging.setJ_content(searchObj);
+			paging.setN_start((page-1)*rows);
+			paging.setN_rows(rows);
+		}
+		return journalismService.selectJournalismService(paging);
 	}
 }
--- src/com/irlab/entity/Journalism.java
+++ src/com/irlab/entity/Journalism.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package com.irlab.entity;
 
 /**
@@ -13,8 +11,8 @@ public class Journalism {
 	private String j_title;//  新闻的标题
 	private String j_time;//  新闻的时间 
 	private String j_digest;//  新闻的摘要
-	private String j_industry;//  新闻的所属行业
-	private String j_url;//  新闻的全文链接
+	private String j_content;//  新闻的全文
+	//private String j_event;//  新闻的所属事件类别
 	public int getJ_id() {
 		return j_id;
 	}
@@ -39,21 +37,15 @@ public class Journalism {
 	public void setJ_digest(String j_digest) {
 		this.j_digest = j_digest;
 	}
-	public String getJ_industry() {
-		return j_industry;
+	public String getJ_content() {
+		return j_content;
 	}
-	public void setJ_industry(String j_industry) {
-		this.j_industry = j_industry;
-	}
-	public String getJ_url() {
-		return j_url;
-	}
-	public void setJ_url(String j_url) {
-		this.j_url = j_url;
+	public void setJ_content(String j_content) {
+		this.j_content = j_content;
 	}
 	@Override
 	public String toString() {
 		return "Journalism [j_id=" + j_id + ", j_title=" + j_title + ", j_time=" + j_time + ", j_digest=" + j_digest
-				+ ", j_industry=" + j_industry + ", j_url=" + j_url + "]";
+				+ ", j_content=" + j_content + "]";
 	}
 }
