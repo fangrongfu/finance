fede2cf7ac9e6039d5539efaec3c7d305d7c80e1
fangrongfu <fangrongfu@DESKTOP-MBOEADA>
2017-09-25 15:36:45 +0800

    添加了季报的接口

--- src/com/irlab/controller/NoticeController.java
+++ src/com/irlab/controller/NoticeController.java
@@ -19,38 +19,50 @@ import com.irlab.entity.Paging;
 import com.irlab.service.impl.NoticeService;
 
 /**
- * @author   fangrongfu
- * @version  1.0
- * @Date     2017年8月11日上午11:23:45
+ * @author fangrongfu
+ * @version 1.0
+ * @Date 2017年8月11日上午11:23:45
  */
 @Controller
 @RequestMapping("/notice")
 public class NoticeController {
-	
+
 	@Resource
 	private NoticeService noticeService;
-	
+
 	@RequestMapping("/select")
 	@ResponseBody
-	public Map<String, Object> selectNoticeControlller(HttpServletRequest request,int page,int rows,String searchObj){
+	public Map<String, Object> selectNoticeControlller(HttpServletRequest request, int page, int rows, String searchObj) {
 		Paging paging = new Paging();
 		Map<String, Object> map = new HashMap<String, Object>();
 		paging.setN_event(searchObj);
 		paging.setN_name(searchObj);
 		paging.setN_code(searchObj);
-		paging.setN_start((page-1)*rows);
+		paging.setN_start((page - 1) * rows);
 		paging.setN_rows(rows);
 		return noticeService.selectNoticeService(paging);
 	}
-	
+
 	@RequestMapping("/selectAll")
 	@ResponseBody
-	public Map<String, Object> selectAllNoticeControlller(HttpServletRequest request,int page,int rows){
-		System.out.println("nihao");
+	public Map<String, Object> selectAllNoticeControlller(HttpServletRequest request, int page, int rows) {
 		Paging paging = new Paging();
 		Map<String, Object> map = new HashMap<String, Object>();
-		paging.setN_start((page-1)*rows);
+		paging.setN_start((page - 1) * rows);
 		paging.setN_rows(rows);
 		return noticeService.selectAllNoticeService(paging);
+	}
+
+	@RequestMapping("/selectDetail")
+	@ResponseBody
+	public Map<String, Object> selectNoticeControlller(HttpServletRequest request, int page, int rows, String n_event,
+			String n_code) {
+		Paging paging = new Paging();
+		Map<String, Object> map = new HashMap<String, Object>();
+		paging.setN_event(n_event);
+		paging.setN_code(n_code);
+		paging.setN_start((page - 1) * rows);
+		paging.setN_rows(rows);
+		return noticeService.selectNoticeService(paging);
 	}
 }
